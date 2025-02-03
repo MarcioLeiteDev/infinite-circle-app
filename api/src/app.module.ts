c3058b6b2970e8496cf4640c76431c069840133b
Marcio Barbosa Leite <marcio.leite@akrkpromotora.local>
2025-02-03 11:09:39 -0300

    dockefile containers

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,23 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { User } from './user/entities/user.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-
+import { User } from './user/entities/user.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root', // Altere para seu usuário do MySQL
-      password: '', // Altere para sua senha
-      database: 'api_nest', // Nome do banco de dados
-      entities: [User], // Inclui as entidades do banco
-      synchronize: true, // Cria as tabelas automaticamente (use só em dev)
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASS'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [User],
+        synchronize: true, // Somente para desenvolvimento
+      }),
     }),
     UserModule,
     AuthModule,
--- api/src/user/entities/user.entity.ts
+++ api/src/user/entities/user.entity.ts
@@ -15,6 +15,36 @@ export class User {
     @Column({ default: 'user' }) // Pode ser "admin", "user", "moderator", etc.
     role: string;
 
+    @Column({ nullable: true })
+    n1: number;
+
+    @Column({ nullable: true })
+    n2: number;
+
+    @Column({ nullable: true })
+    n3: number;
+
+    @Column({ nullable: true })
+    n4: number;
+
+    @Column({ nullable: true })
+    n5: number;
+
+    @Column({ nullable: true })
+    n6: number;
+
+    @Column({ nullable: true })
+    n7: number;
+
+    @Column({ nullable: true })
+    n8: number;
+
+    @Column({ nullable: true })
+    n9: number;
+
+    @Column({ nullable: true })
+    n10: number;
+
     @Column()
     password: string;
     @BeforeInsert()
